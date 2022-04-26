e0f315eca1246302270dceb41e2b3ae90bbf52e2
localhousee <ananditodwiky666@gmail.com>
2022-04-26 11:28:35 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
