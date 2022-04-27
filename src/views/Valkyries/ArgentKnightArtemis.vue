494e0b87a7f5060c6506a93a1a0ec0928dab0203
localhousee <ananditodwiky666@gmail.com>
2022-04-27 11:56:45 +0700

    Update info VG, TP, SnS, AKA

--- src/views/Valkyries/ArgentKnightArtemis.vue
+++ src/views/Valkyries/ArgentKnightArtemis.vue
@@ -26,26 +26,31 @@
 							{
 								id: 1,
 								name: 'Blessing of Winter Harvest',
-								priority: 'Start',
+								desc: 'Enemies recovering from Ice Cell are inflicted with 1 stack of Hypothermia',
+								priority: 'Start'
 							},
 							{
 								id: 2,
 								name: 'Blessing of Stellar Trials',
+								desc: 'Inflicting Ice Cell also creates a Frost Ring centered around the target lasting 2s which deals 480% ATK of Ice DMG per 0.3s to enemies nearby',
 								priority: '1st'
 							},
 							{
 								id: 3,
 								name: 'Blessing of Frost Scythe',
+								desc: 'Tap [ATK] after evasion to hurl Frost Guisarmier which spins for 5s, gathers enemies nearby, and deal 5400% ATK of Ice DMG. CD: 10s',
 								priority: '2nd'
 							},
 							{
 								id: 4,
 								name: 'Blessing of Uranian Mirror',
+								desc: 'Burst mode consumes 100% more SP and Valkyrie deals 90% bonus Total DMG in burst mode',
 								priority: 'Filler'
 							},
 							{
 								id: 5,
 								name: 'Blessing of Cold Moon',
+								desc: 'Inflicting Ice Cell also creates a self-centered Frost Ring lasting 6s which deals 180% ATK of Ice DMG per 0.3s to enemies nearby',
 								priority: 'Optional'
 							}
 						],
@@ -57,16 +62,19 @@
 							{
 								id: 1,
 								name: 'Lip Poison [E]',
+								desc: 'Total DMG from summoned entities, ELFs, and Support Valkyries increases by 60%. ELFs',
 								priority: '1st',
 							},
 							{
 								id: 2,
 								name: 'Lodging [C]',
+								desc: 'ELF Ultimates and Support Valkyries support skills have 20% reduced CD',
 								priority: '2nd'
 							},
 							{
 								id: 3,
 								name: 'Entwined [P]',
+								desc: 'For every summoned entity, ELF, or Support Valkyrie present, enemies take 8% bonus Total DMG (5 stacks max)',
 								priority: '3rd'
 							},
 							{
@@ -77,11 +85,13 @@
 							{
 								id: 5,
 								name: 'Dead [X]',
+								desc: 'For every second in Mind Sync, Valkyrie herself, summoned entities, ELFs, and Support Valkyries deal 10% bonus Total DMG. 5 stacks max. 9 stacks max with any summoned entity present. Reset on exiting Mind Sync',
 								priority: 'YES'
 							},
 							{
 								id: 6,
 								name: 'Newborn [X]',
+								desc: 'Triggering Mind Sync again during Mind Sync no longer resets the duration but extends the duration by 4.0s instead. With any summoned entity present, the duration is extended by 5s additionally',
 								priority: 'Filler'
 							}
 						],
@@ -93,31 +103,37 @@
 							{
 								id: 1,
 								name: 'Rochet of the Pilgrim',
+								desc: 'Ultimate restores 3 sp per second for 5s',
 								priority: 'YES'
 							},
 							{
 								id: 2,
 								name: 'Artifact of the Inhibitor',
+								desc: 'Ultimate boosts Elemental Breach by 25% for 5s',
 								priority: '1st'
 							},
 							{
 								id: 3,
 								name: 'Brand of the Undead',
+								desc: 'Ultimate boosts Elemental DMG by 30% for 5s',
 								priority: '2nd'
 							},
 							{
 								id: 4,
 								name: 'Goblet of the Giver',
+								desc: 'Ultimate boosts Total DMG by 35% for 5s',
 								priority: '3rd'
 							},
 							{
 								id: 5,
 								name: 'Shadow of the Deliverer',
-								priority: null
+								desc: '',
+								priority: ''
 							},
 							{
 								id: 6,
 								name: 'Dream of the Deliverer',
+								desc: 'While War of the Deliverer is active increase normal signets of Deliverance effects by 50%',
 								priority: 'YES'
 							},
 						],
@@ -129,31 +145,37 @@
 							{
 								id: 1,
 								name: 'Recitatif of Creeks',
+								desc: 'Every 10 SP left boosts Physical & Elemental DMG by 2.5%',
 								priority: '1st'
 							},
 							{
 								id: 2,
 								name: 'Recitatif of Eden',
+								desc: 'Every 10 SP left boosts Total DMG by 3.0%',
 								priority: '2nd'
 							},
 							{
 								id: 3,
 								name: 'Recitatif of Fine Jade',
+								desc: 'Initial SP and max SP increase by 30',
 								priority: '3rd'
 							},
 							{
 								id: 4,
 								name: 'Echo of Gold',
-								priority: null
+								desc: '',
+								priority: ''
 							},
 							{
 								id: 5,
 								name: 'Echo of Silent Night',
+								desc: 'After lasting over 7s in Final Note, reduce ATK, DEF, ele resists of enemies nearby',
 								priority: '1st'
 							},
 							{
 								id: 6,
 								name: 'Echo of Withered Soil',
+								desc: 'After lasting over 7s in Final Note, boost SP cap and restore SP/s till it ends',
 								priority: 'Filler'
 							},
 						],
@@ -165,26 +187,30 @@
 							{
 								id: 1,
 								name: 'Motto of Dibba-Cakkhu',
+								desc: 'Combo hits are reset at to boost Total DMG taken by all enemies by 35% for 10s',
 								priority: '1st'
 							},
 							{
 								id: 2,
 								name: 'Motto of Asavakkhaya',
+								desc: 'Attacks grant 25 bonus combo hits on hit. CD: 5s',
 								priority: '2nd'
 							},
 							{
 								id: 3,
 								name: 'Motto of Iddhi-Vidha',
+								desc: 'Combo hits are increased to 60 when below 60. CD: 10s',
 								priority: '3rd'
 							},
 							{
 								id: 4,
 								name: 'Dictum of Bodhi',
-								priority: null
+								priority: ''
 							},
 							{
 								id: 5,
 								name: 'Dictum of Anatman',
+								desc: 'Bodhi Open Eye debuffs last 3 seconds longer and every debuff on the enemy boosts Total DMG taken by 5%',
 								priority: 'Optional'
 							},
 						],
